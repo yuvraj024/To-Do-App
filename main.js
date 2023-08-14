917af682eea8ac4d1ea2a2f0dfe631cf3fe417a6
Yuvraj <oberoiyuvraj024@gmail.com>
2023-08-14 13:28:52 +0530

    Added the delete button functionality which was not there previously

--- main.js
+++ main.js
@@ -11,11 +11,18 @@ function addItem(e){
     // get input value
     const newItem=document.getElementById("item").value;
 
-
     // create new li element
     const li=document.createElement("li");
     li.className="list-group-item";
     // add text node with input value
     li.appendChild(document.createTextNode(newItem)); 
+
+    // Create the delete button element
+    const deleteBtn=document.createElement("button");
+    deleteBtn.className="btn btn-danger btn-sm float-right delete";
+    deleteBtn.appendChild(document.createTextNode("X"));
+    
+    //  append the button to li and li to main ul
+    li.appendChild(deleteBtn);
     itemList.appendChild(li);
 }
