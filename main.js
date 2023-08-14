fa2ded8357f8eb56a8488c32a7a70f8376fa0a62
Yuvraj <oberoiyuvraj024@gmail.com>
2023-08-14 13:49:32 +0530

    Added search functionality for the app

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const form=document.getElementById("addForm");
 const itemList=document.getElementById("items");
+const filter=document.getElementById("filter");
 
 // Form submit event
 form.addEventListener("submit",addItem);
 // Delete Event
 itemList.addEventListener("click",removeItem);
-
+//Search Event
+filter.addEventListener("keyup",filterItems);
 // Add item
 function addItem(e){
     e.preventDefault();
@@ -34,8 +36,27 @@ function addItem(e){
 function removeItem(e){
     if(e.target.classList.contains("delete")){
         if(confirm("Are You Sure?")){
-            const li=e.target.parentElement;
+            const li=e.target.parentElement;    //bcoj target is btn(child to li)
             itemList.removeChild(li);
         }
     }
+}
+
+
+// Filter Items
+
+function filterItems(e){
+     //convert text to lower case
+     const text=e.target.value.toLowerCase();
+    //  get list of elements
+     const items=itemList.getElementsByTagName("li");
+    //  convert to Array
+    Array.from(items).forEach(function(item){
+        const itemName=item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!=-1){
+            item.style.display="block";
+        }else{
+            item.style.display="none";
+        }
+    });
 }
